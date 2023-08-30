648eb9211507d375998634a9e837709aa32dd618
Illia Smyrnov <ilushasm@gmail.com>
2023-08-30 23:58:56 +0300

    Added Book model

--- book/models.py
+++ book/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+
+class Book(models.Model):
+    class BookCover(models.TextChoices):
+        SOFT = "SOFT"
+        HARD = "HARD"
+
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    inventory = models.PositiveIntegerField()
+    cover = models.CharField(max_length=4, choices=BookCover.choices)
+    daily_fee = models.DecimalField()
